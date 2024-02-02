e7ad9e9ae9842f302106356dbc14258255bd0870
Zoe Tucker <zoetucker@library.ucla.edu>
2024-02-01 17:26:15 -0800

    more tests

--- signs/tests.py
+++ signs/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from signs.models import Location
-from signs.views_utils import construct_display_url
+from signs.views_utils import construct_display_url, get_hours, get_start_end_dates
 
 # Create your tests here.
 
@@ -22,3 +22,35 @@ class ConstructDisplayURLTestCase(TestCase):
         powell = Location.objects.get(location_id=1)
         url = construct_display_url(powell.location_id, "portrait_small")
         self.assertEqual(url, "/display/1/portrait_small")
+
+
+class GetStartEndDatesTestCase(TestCase):
+    def test_get_start_end_dates(self):
+        hours = [
+            {"date": "2024-01-29", "weekday": "Monday", "rendered_hours": "8am - 5pm"},
+            {
+                "date": "2024-01-30",
+                "weekday": "Tuesday",
+                "rendered_hours": "8am - 5pm",
+            },
+            {
+                "date": "2024-01-31",
+                "weekday": "Wednesday",
+                "rendered_hours": "8am - 5pm",
+            },
+            {
+                "date": "2024-02-01",
+                "weekday": "Thursday",
+                "rendered_hours": "8am - 5pm",
+            },
+            {"date": "2024-02-02", "weekday": "Friday", "rendered_hours": "8am - 3pm"},
+            {
+                "date": "2024-02-03",
+                "weekday": "Saturday",
+                "rendered_hours": "Closed",
+            },
+            {"date": "2024-02-04", "weekday": "Sunday", "rendered_hours": "Closed"},
+        ]
+        start, end = get_start_end_dates(hours)
+        self.assertEqual(start, "Jan 29")
+        self.assertEqual(end, "Feb 04")
