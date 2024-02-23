dda7ec8973cd9ed53cb43937f6a4ab226a1ce8ac
ztucker4 <zoetucker@library.ucla.edu>
2024-02-23 13:25:22 -0800

    SYS-1538: fix incorrect URL scheme in production (#11)

--- project/settings.py
+++ project/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+RUN_ENV = os.getenv("DJANGO_RUN_ENV")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
